33cb81b9511bb793422dcababe19eb309c13bb1a
Martin <martindu@umich.edu>
2014-12-30 18:33:53 -0800

    Add and Delete item title done

--- MyToDoList App/FirstViewController.swift
+++ MyToDoList App/FirstViewController.swift
@@ -8,18 +8,81 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+//var item : [(title: String, detail: String)?] = []
 
+var item:[String] = [ ]
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tasksTable:UITableView?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    
+            return item.count
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    var arr = ["martin","dureja"]
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+    
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        
+        var ex = item[indexPath.row]
+        
+        println("Ex value is: \(ex)" )
+        
+        cell.textLabel?.text = ex
+        
+        return cell
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        
+        
+        if var storedToDoItems: AnyObject = NSUserDefaults.standardUserDefaults().objectForKey("toDoTitle"){
+            
+            item = []
+            
+            for( var i = 0; i < storedToDoItems.count; i++ ){
+            
+                item.append(storedToDoItems[i] as NSString)
+            }
+        
+        }
+        
+        tasksTable?.reloadData()
+    }
+    
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+    
+        if( editingStyle == UITableViewCellEditingStyle.Delete){
+            
+            item.removeAtIndex(indexPath.row)
+            
+            let fixedToDoItems = item
+            
+            NSUserDefaults.standardUserDefaults().setObject(fixedToDoItems, forKey: "toDoTitle")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+            tasksTable?.reloadData()
+            
+        }
+    }
 
 
 }
-
--- MyToDoList App/SecondViewController.swift
+++ MyToDoList App/SecondViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var enteredTitle: UITextField!
+    
+    @IBOutlet var enteredDetail: UITextField!
+    
+    @IBAction func pressedButton(sender: AnyObject) {
+    
+        var _title: String = " "
+        var _detail: String = " "
+    
+        _title = enteredTitle.text!
+        _detail = enteredDetail.text!
+        
+        //item.append((title: _title,detail: _detail))
+        item.append(enteredTitle.text)
+        
+        let fixedToDoItems = item
+        
+        NSUserDefaults.standardUserDefaults().setObject(fixedToDoItems, forKey: "toDoTitle")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        //var storedToDoItems: AnyObject? = NSUserDefaults.standardUserDefaults().objectForKey("toDoTitle")
+     
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //println( NSUserDefaults.standardUserDefaults().objectForKey("toDoTitle") )
     }
 
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    } // called when 'return' key pressed.Toggles down the keyboard
+    
+     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    } // toggles down keyboard when user touches anywhere on screen
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
